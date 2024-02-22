e01ad0ce082fe5c3d092091e44aa1bb356e9cff9
Nahum Velazquez <vgjn98x@gmail.com>
2024-02-22 11:31:56 -0600

    first commit

---
+++ Desafios6.js
@@ -0,0 +1,17 @@
+/**
+ * 1 - Crear una funcion que muestre Hola mundo en la consola
+ */
+
+alert('Funcion Hola mundo');
+
+function HolaMundo() {
+    console.log("Hola mundo");
+}
+
+HolaMundo();
+
+/**
+ * 2 - Crear una funcion que reciba un nombre como parametro y muestre Hola nombre
+ * en la consola
+ */
+
